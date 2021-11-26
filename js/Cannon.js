54a5cdca4222ff2ea7b292c04f852e6f6b78d26a
vishalsuguna <90913499+vishalsuguna@users.noreply.github.com>
2021-11-26 18:58:20 +0530

    Add files via upload

---
+++ js/Cannon.js
@@ -0,0 +1,32 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+  }
+  display() {
+    console.log(this.angle)
+    // change the angle in the upward direction when the left arrow key was pressed
+   //  changed the angle in the downward direction when the right arrow key was pressed
+    if(keyIsDown(UP_ARROW)&this.angle>-1.45)
+    {
+     this.angle-=0.2
+    }
+
+    if(keyIsDown(DOWN_ARROW)&this.angle<0.35)
+    {
+     this.angle+=0.2
+    }
+    
+    fill("#676e6a");
+    push();
+    translate(this.x, this.y);
+    rotate(this.angle);
+    rect(-10, -20, this.width, this.height);
+    pop();
+    arc(this.x - 30, this.y + 90, 140, 200, PI, TWO_PI);
+    noFill();
+  }
+}
